e57a7ecee06737f5e267864011256ad1e88c110a
Mallek77 <35771265+Mallek77@users.noreply.github.com>
2026-06-06 17:33:46 +0200

    Update API_URL in MissionsContext.tsx

--- src/store/MissionsContext.tsx
+++ src/store/MissionsContext.tsx
@@ -1,6 +1,6 @@
 import { createContext, useContext, useState, useEffect, ReactNode } from "react";
 
-const API_URL = "https://script.google.com/macros/s/AKfycbyqxkK7jtGi52WZ4klbxnHskmvIrDH5-AwkA_pCemQ7SHSCeZtF5IVYn2dXpxLdgGOe/exec";
+https://script.google.com/macros/s/AKfycbzWKE1a7M8P9lBSCP1kEaxDR8mZsJLhZL2Lm2R1p7SoDdomi3yXXArsqokFmLkI4vfi/exec
 
 export type Mission = {
   id: string;
